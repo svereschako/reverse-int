a59a0e66ea8878298f70d9866c2d59a657c9e618
svereschako <svereschako@gmail.com>
2022-01-12 18:22:18 +0300

    feat: add function in index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+  var num_string = n.toString();
+  var array = num_string.split("");
+  var reverse_arr = array.reverse();
+  var res = reverse_arr.join("");
+  res = Number.parseInt(res);
+  return res;
 }
+
+
+
+
